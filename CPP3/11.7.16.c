f48aa9faa9fc901c5fd6f0ed4fdf9b17a9877c08
lin <lin.04290619@gmail.com>
2023-11-29 20:02:02 +0800

    correct

--- CPP3/11.7.16.c
+++ CPP3/11.7.16.c
@@ -1,47 +1,62 @@
-#include<stdio.h>
-const int size=100;
+#include <stdio.h>
+#include <ctype.h>
+const int size = 100;
 void print(char *s);
 void upper(char *s);
 void lower(char *s);
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
     char c;
-    if(argc==1) c='p';
-    else c==argv[1][1];
+    if (argc == 1)
+        c = 'p';
+    else
+        c = argv[1][1];
     char text[size];
-    fgets(text,size,stdin);
-    switch(c){
-        case 'p':
-        case 'P':
-            print(text);
-            break;
-        case 'u':
-        case 'U':
-            upper(text);
-            break;
-        case 'l':
-        case 'L':
-            lower(text);
-            break;        
+    fgets(text, size, stdin);
+    switch (c)
+    {
+    case 'p':
+    case 'P':
+        print(text);
+        break;
+    case 'u':
+    case 'U':
+        upper(text);
+        break;
+    case 'l':
+    case 'L':
+        lower(text);
+        break;
     }
     return 0;
 }
 
-void print(char *s){
-    printf("%s",s);
+void print(char *s)
+{
+    printf("%s", s);
 }
-void upper(char *s){
-    while(*s != EOF && *s !='\0'){
-        *s+=32;
+void upper(char *s)
+{
+    while (*s != EOF)
+    {
+        if(isalpha(*s))
+            *s -= 32;
+        putchar((int)*s);
         s++;
-        putchar(*s);
+        if (*s == '\n')
+            break;
     }
 }
-void lower(char *s){
-    while(*s != EOF && *s !='\0'){
-        *s-=32;
+void lower(char *s)
+{
+    while (*s != EOF)
+    {
+        if(isalpha(*s))
+            *s += 32;
+        putchar((int)*s);
         s++;
-        putchar(*s);
+        if (*s == '\n')
+            break;
     }
 }
